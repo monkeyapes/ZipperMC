0636700fc9ed7bf7c71f46cfb29175608e531b55
ZipperMC <zippermc@dev.com>
2026-07-27 22:18:49 +0300

    v1.5.5: added 7 more official Mojang package names
    New packages added: com.mojang.minecrafttrialpe, com.mojang.minecraftpe.demo,
    com.mojang.minecraftearth, com.mojang.minecraftonline,
    com.mojang.minecraftedu_preview, com.mojang.scrolls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.zippermc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.5.4"
+        versionCode = 14
+        versionName = "1.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
